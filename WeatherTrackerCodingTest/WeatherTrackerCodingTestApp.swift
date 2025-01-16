dfcc5f07937ca86db2da9a4822f8811b50dcf94a
GLOBAL\v747369 <suraj.dhopati@united.com>
2025-01-16 14:44:27 -0500

    Initial Commit

---
+++ WeatherTrackerCodingTest/WeatherTrackerCodingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerCodingTestApp.swift
+//  WeatherTrackerCodingTest
+//
+//  Created by Suraj Raju Dhopati on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerCodingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherTrackerCodingTestTests/WeatherTrackerCodingTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerCodingTestTests.swift
+//  WeatherTrackerCodingTestTests
+//
+//  Created by Suraj Raju Dhopati on 1/16/25.
+//
+
+import Testing
+@testable import WeatherTrackerCodingTest
+
+struct WeatherTrackerCodingTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
